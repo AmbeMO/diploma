8679896ab7f59d04ef83181938069d8844fe4c87
AmbeMO <milamo@mail.ua>
2021-05-26 22:51:38 +0300

    add layout

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ReceipePageComponent } from './receipe-page/receipe-page.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    HomePageComponent,
+    ReceipePageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
