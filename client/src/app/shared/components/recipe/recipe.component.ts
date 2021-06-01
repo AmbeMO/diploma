70814ab73d2805554b1f2bb2bc8ce0f8433eb0ae
AmbeMO <milamo@mail.ua>
2021-06-02 00:28:52 +0300

    add back btn and reefactor get req

--- client/src/app/shared/components/recipe/recipe.component.ts
+++ client/src/app/shared/components/recipe/recipe.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Recipe} from '../../interfaces';
 import {Subscription} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
@@ -10,25 +10,39 @@ import {RecipeService} from '../../recipe.service';
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss']
 })
-export class RecipeComponent implements OnInit {
+export class RecipeComponent implements OnInit , OnDestroy {
 
   recipes: Recipe[] = [];
+  pSub: Subscription
 
   searchStr = '';
 
 
-  constructor( private http: HttpClient) { }
+  // constructor( private http: HttpClient) { }
 
-  ngOnInit() {
-    this.http.get<Recipe[]>(`http://localhost:8080/recipe`)
-        .subscribe(recipes => {
-          console.log(' 1 response', recipes)
-          this.recipes = recipes
-        })
+  // ngOnInit() {
+  //   this.http.get<Recipe[]>(`http://localhost:8080/recipe`)
+  //       .subscribe(recipes => {
+  //         console.log(' 1 response', recipes)
+  //         this.recipes = recipes
+  //       })
+  //
+  // }
 
-  }
+constructor(private recipeService: RecipeService) {
+}
 
+    ngOnInit() {
+    this.pSub = this.recipeService.getAll().subscribe(recipes => {
+        this.recipes = recipes
+    })
+}
 
+ngOnDestroy() {
+    if (this.pSub) {
+        this.pSub.unsubscribe()
+    }
+}
 
     // @Input() recipe: Recipe
 
